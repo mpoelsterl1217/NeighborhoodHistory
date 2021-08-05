c357a552022d99750ffc8882673b7de9dae0e536
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-08-05 13:10:24 -0500

    ARView progress: sphere appears in scene

--- NeighborhoodHistory/ViewControllers/ARViewController.swift
+++ NeighborhoodHistory/ViewControllers/ARViewController.swift
@@ -6,13 +6,27 @@
 //
 
 import UIKit
+import RealityKit
 
 class ARViewController: UIViewController {
-
+    
+    //outlets
+    @IBOutlet weak var arView: ARView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        //Create 3D Ball
+        let sphere = MeshResource.generateSphere(radius: 0.1)
+        let material = SimpleMaterial(color: .black, roughness: 0, isMetallic: true)
+        
+        let sphereEntity = ModelEntity(mesh: sphere, materials: [material])
+        
+        //Create anchor
+        let sphereAnchor = AnchorEntity(world: SIMD3(x: 0, y: 0, z: 0))
+        sphereAnchor.addChild(sphereEntity)
+        
+        //Add anchor to scene
+        arView.scene.addAnchor(sphereAnchor)
     }
     
 
