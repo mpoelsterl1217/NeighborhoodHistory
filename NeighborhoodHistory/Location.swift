9164026328b0139c60f666016baeceabf6a035d1
Matthew Poelsterl <mpoelsterl1217@gmail.com>
2021-08-05 12:34:40 -0500

    Created ARview controller file

--- NeighborhoodHistory/Location.swift
+++ NeighborhoodHistory/Location.swift
@@ -8,10 +8,11 @@
 import Foundation
 import MapKit
 
-class Location: NSObject, MKAnnotation {
+class Location: NSObject, MKAnnotation, Codable {
     let name: String
     let address: String?
-    let coordinate: CLLocationCoordinate2D
+    let latitude: Double
+    let longitude: Double
     let caption: String?
     let descript: String?
     let tags: [String]?
@@ -19,7 +20,8 @@ class Location: NSObject, MKAnnotation {
     init(name: String, address: String?, coordinate: CLLocationCoordinate2D, caption: String?, descript: String?, tags: [String]?) {
         self.name = name
         self.address = address
-        self.coordinate = coordinate
+        self.latitude = coordinate.latitude
+        self.longitude = coordinate.longitude
         self.caption = caption
         self.descript = descript
         self.tags = tags
@@ -35,6 +37,9 @@ class Location: NSObject, MKAnnotation {
     }
     var markerTintColor: UIColor {
         return .orange
+    }
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: self.latitude, longitude: self.longitude)
     }
     
 }
